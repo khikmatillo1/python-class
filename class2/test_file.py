b5b332d35b29d3104ca8f9886283b15e25db5474
khikmatillo1 <twaravancargo@gmail.com>
2024-05-20 20:37:42 -0500

    added class2

---
+++ class2/test_file.py
@@ -0,0 +1,112 @@
+#!workspace/bin/python
+# """
+#     This is a documentation
+# """
+# A  =  5
+# B  = 10
+# C = A + B
+
+# print(C)
+
+# FRUIT = "apple"
+
+# #Find out methods for strings 
+# print(dir(FRUIT)) 
+
+# #Find out type of variable 
+# print(type(FRUIT)) 
+
+# #Lean Methods
+# print(FRUIT.capitalize())
+# print(FRUIT.isdigit())
+# print(FRUIT.upper())
+# print(FRUIT.lower()) 
+
+# RESPONSE = input("Please enter your name:") 
+
+# #strip() methon removes spaces from both sides  
+# print("Hello", RESPONSE.strip())
+
+# #strip() methon removes spaces from left sides
+# print("Hello", RESPONSE.lstrip())
+
+# #strip() methon removes spaces from right sides
+# print("Hello", RESPONSE.rstrip())  
+
+# CLEANED_RESPONSE = RESPONSE.lstrip() 
+# CLEANED_EVEN_MORE = CLEANED_RESPONSE.rstrip() 
+# LOWERED = CLEANED_EVEN_MORE.lower()
+# print(LOWERED)
+
+
+
+# RESPONSE = input("Please enter your phone number:")  
+# print("Phone number", RESPONSE)
+
+# RESPONSE = input("Please enter your email:")  
+# print("Email:", RESPONSE)
+
+# Integer 
+# NUMBER1 = 5 
+# NUMBER2 = 10  
+
+# print (type(NUMBER1))
+# print(dir(NUMBER1)) 
+
+# RESPONSE1 = NUMBER1 + NUMBER2 
+# NUMBER1 * NUMBER2 
+# NUMBER2 / NUMBER1
+# NUMBER2 - NUMBER1 
+
+# # Floats 
+# NUMBER4 = 2.0 
+# print(type(NUMBER4)) 
+
+
+# List 
+# FRUIT = ["apple","pear", "orange", "apple", "apricot"] 
+
+# print=(FRUIT.count("apple"))
+
+# print(type(FRUIT)) 
+
+# print(FRUIT)
+
+# print(dir(FRUIT))
+
+
+
+# FRUIT.append("melon") 
+
+# FRUIT.count("apple")
+
+# FRUIT.clear()
+
+# print(FRUIT)
+
+
+# Tuple 
+# FRUIT = ("apple", "pear")
+# print(dir(FRUIT)) 
+# print(type(FRUIT))  
+
+
+
+# Maps also disctionary 
+
+# FRUIT = {
+#     "big": "apple",
+#     "sweet": "melon" 
+# }
+
+# print(dir(FRUIT))
+# print(type(FRUIT)) 
+
+# print(FRUIT["big"]) 
+
+
+
+# GREETINGS = "hello" 
+# RESPONSE = input("Please enter your name: ")
+
+# print(GREETINGS + " " + RESPONSE) 
