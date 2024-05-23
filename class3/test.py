5c4106ff12f312725d69ca78801cef2990e4b66f
khikmatillo1 <twaravancargo@gmail.com>
2024-05-22 20:31:45 -0500

    added class3

---
+++ class3/test.py
@@ -0,0 +1,111 @@
+#!workspace/bin/python 
+
+# Logic  
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+
+# if NUMBER1 is NUMBER2:
+#     print("They are equal")
+# else:
+#     print("They are not equal") 
+
+
+# NUMBER1 = 5 
+# NUMBER2 = 10
+
+# # If sentence 
+
+# if NUMBER1 == NUMBER2: 
+#     print("They are equal")  
+# elif NUMBER2 == NUMBER1:
+#     print("Of course they are equal ")
+# else: 
+#     print("They are not equal") 
+
+
+# Nester if sentence 
+
+# FRUIT = "apple"
+# COLOR = "red" 
+# SIZE  = "big"
+
+# if FRUIT == "apple":
+#     print("the fruit is apple")  
+#     if COLOR == "red":
+#         print("the color is red")
+#         if SIZE == "big":
+#             print("the fruit is big") 
+
+# import os
+
+# FILENAME = os.path.isfile("/etc/passwd")
+
+# if FILENAME: 
+#     print("the file exists")
+# else: 
+#     print("the file does not exist") 
+
+
+# RESPONSE = int() 
+
+# try: 
+#     RESPONSE = int(input("when were you born: "))
+# except ValueError: 
+#     print("only digits are accepted. Please type number e.g 2000") 
+
+# RESPONSE = int(input("When were you born: "))
+
+# if RESPONSE >= 2025:
+#     print("You were not born yet")
+
+# elif RESPONSE >= 2000 and RESPONSE <= 2024:
+#     print("You were born in 21st centery")
+
+# elif RESPONSE <= 1999 and RESPONSE >= 1990:
+#     print("you are in your 30th") 
+
+# elif RESPONSE <=1989 and RESPONSE >= 1980: 
+#     print("you are in your 40th ")
+
+# else:  
+#     print("try again") 
+
+# in operator  
+# HIDDEN_FRUIT = "banana"
+
+# FRUIT_LIST1 = ["apple", "mango", "banana"]  
+
+# FRUIT_LIST2 = ["pear", "melon","apricot"]
+
+# FRUIT_LIST3 = ["peach", "grape", "kiwi"]
+
+# if HIDDEN_FRUIT in FRUIT_LIST1:
+#     print("%s is in LIST1" % HIDDEN_FRUIT)  
+# elif HIDDEN_FRUIT in FRUIT_LIST2:
+#     print("%s is in List2" %HIDDEN_FRUIT) 
+# elif HIDDEN_FRUIT in FRUIT_LIST3:
+#     print("%s is in List3" %HIDDEN_FRUIT)
+# else:
+#     print("%s is not found" %HIDDEN_FRUIT)
+
+
+# %s string 
+# %d digit 
+
+
+
+
+
+
+
+
+
+
+
+
+
